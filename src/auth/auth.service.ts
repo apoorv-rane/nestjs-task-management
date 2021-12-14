176bd849e15993fb8042270c9e49376d46e6f8cb
apoorv-neosofttech <apoorv.rane@neosoftmail.com>
2021-12-14 18:29:47 +0530

    Added signup

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { UsersRepository } from './users.repository';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(UsersRepository) private usersRepository: UsersRepository,
+  ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    await this.usersRepository.createUser(authCredentialsDto);
+  }
+}
